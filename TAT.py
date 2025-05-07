e814475098d945b6df531f0e3652915face92d73
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2025-05-07 12:22:15 +0300

    Update TAT.py

--- TAT.py
+++ TAT.py
@@ -146,12 +146,7 @@ def fetch_and_process_data(file_input):
         'Service_Bill_Date_Time',
         'Procedure_Completion_Date_'
     ]
-    for col in datetime_columns:
-        if col in TAT_df.columns:
-            st.write(f"**{col} dtype:**", str(TAT_df[col].dtype))
-            st.write(TAT_df[col].head())
-        else:
-            st.write(f"**{col}** not found in CSV.")
+
 
     # Select columns for Overall TAT calculation
     overall_columns = [
